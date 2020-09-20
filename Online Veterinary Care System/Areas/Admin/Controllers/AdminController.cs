8c76b3b2e82779706f82204d8882137500820c23
Arif Najar <najararifahmad@gmail.com>
2020-09-20 12:25:40 +0530

    Theme included

---
+++ Online Veterinary Care System/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Online_Veterinary_Care_System.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Boote Nasal Admin");
+        }
+    }
+}
