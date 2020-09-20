ed6675e54b4c9d5f4fa34395da5976997f8ae6a2
Arif Najar <najararifahmad@gmail.com>
2020-09-20 21:26:23 +0530

    Login/ Register Page UI Complete

---
+++ Online Veterinary Care System/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace Online_Veterinary_Care_System.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Online Veterinary Care System/assets/js/appLogic.js
@@ -0,0 +1,48 @@
+﻿function isLoggedIn() {
+    const token = localStorage.getItem('token');
+    if (token === null || token === '' || token === undefined) {
+        return false;
+    }
+    const currentDate = new Date();
+    const tokenExpirationDate = new Date(JSON.parse(localStorage.getItem('token'))['.expires']);
+    if (currentDate > tokenExpirationDate) {
+        return false;
+    }
+    return true;
+}
+
+function isAdminLoggedIn() {
+    const token = localStorage.getItem('token');
+    if (token === null || token === '' || token === undefined) {
+        return false;
+    }
+    const currentDate = new Date();
+    const tokenExpirationDate = new Date(JSON.parse(localStorage.getItem('token'))['.expires']);
+    if (currentDate > tokenExpirationDate) {
+        return false;
+    }
+
+    if (JSON.parse(localStorage.getItem('token')).role === 'Admin') {
+        return true;
+    }
+
+    return false;
+}
+
+function isDoctorLoggedIn() {
+    const token = localStorage.getItem('token');
+    if (token === null || token === '' || token === undefined) {
+        return false;
+    }
+    const currentDate = new Date();
+    const tokenExpirationDate = new Date(JSON.parse(localStorage.getItem('token'))['.expires']);
+    if (currentDate > tokenExpirationDate) {
+        return false;
+    }
+
+    if (JSON.parse(localStorage.getItem('token')).role === 'Doctor') {
+        return true;
+    }
+
+    return false;
+}
